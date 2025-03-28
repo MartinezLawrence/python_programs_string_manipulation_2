1ea23d3122146dce273a410a94b1babf4b2e333f
unknown <lawrencekulet@gmail.com>
2025-03-28 16:21:39 +0800

    pseudocode

---
+++ remove_leading_spaces_without_lstrip.py
@@ -0,0 +1,7 @@
+# remove leading spaces without lstrip
+# this program will remove leading spaces without lstrip
+
+# initialize a pointer to 0 
+# if the pointer is less than the length of the string and the character at the pointer is a space
+# increment the pointer 
+# return the string from the pointer to the end of the string_
