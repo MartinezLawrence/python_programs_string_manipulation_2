38db3fed79348ae6885ba175387fa5585126d0c6
unknown <lawrencekulet@gmail.com>
2025-03-28 18:45:15 +0800

    check if all uppercase finished

--- check_if_all_uppercase_without_isupper.py
+++ check_if_all_uppercase_without_isupper.py
@@ -2,7 +2,16 @@
 # this program will check if all characters is on uppercase without isupper()
 
 # ask the user for a string
+string = input("Enter a string: ")
+
 # initialize a flag all_uppercase to True
+all_uppercase = True
+
 # iterate through each character in the string
-# if the character is not uppercase, set all_uppercase to False and break loop
+for char in string: 
+    if 'a' <= char <= 'z':
+        all_uppercase = False  # if the character is lowercase, set all_uppercase to False
+        break
+
 # print whether all_uppercase is True or False
+print(all_uppercase)
