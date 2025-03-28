06799dd51ce652d7e28074ff935db37283c487b3
unknown <lawrencekulet@gmail.com>
2025-03-28 19:26:47 +0800

    pseudocode

---
+++ title_case_without_title().py
@@ -0,0 +1,8 @@
+# title case without title()
+# this program will makes all first letter of each word in the string capital letter
+
+# ask the user for a string
+# split the string into words
+# for each word, capitalize the first letter and make the rest lowercase
+# join the modified words into a single string
+# print the result
