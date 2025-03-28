084379a738cf71b80904a55dac3cd87290404651
unknown <lawrencekulet@gmail.com>
2025-03-28 19:23:57 +0800

    capitalize without capitalize() finished

--- capitalize_without_capitalize().py
+++ capitalize_without_capitalize().py
@@ -2,6 +2,12 @@
 # this program will makes the first letter of the string capital letter
 
 # ask the user for a string
+string = input("Enter a string: ")
+
 # check if the string is empty
+if string: 
+    print(string[0].upper() + string[1:].lower(), end="")
+
 # if not empty, convert the first letter to uppercase and the rest to lowercase
-# print the result
+else: 
+    print(string, end="")   # # print the result
