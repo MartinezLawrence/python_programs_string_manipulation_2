2a5a234dccdac48cd9fcf5a3bbb1cdc3487b2151
unknown <lawrencekulet@gmail.com>
2025-03-28 16:56:15 +0800

    remove characters without removeprefix

--- remove_characters_before_string_without_removeprefix.py
+++ remove_characters_before_string_without_removeprefix.py
@@ -2,6 +2,11 @@
 # this program will remove characters at the beginning of a string without removeprefix
 
 # ask the user for a string and a prefix
-# check if the prefix is in the string
-# if it is, remove the prefix from the string
-# otherwise, print the string as is
+string = input("Enter a string: ")
+prefix = input("Enter a prefix: ")
+
+if string.startswith(prefix): # check if the prefix is in the string
+    print(string[len(prefix):])  # if it is, remove the prefix from the string
+
+else: # otherwise, print the string as is
+    print(string) 
