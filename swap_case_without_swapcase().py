20d33a7ff13143a1b6a6a15e7e37f5f631649448
unknown <lawrencekulet@gmail.com>
2025-03-28 19:20:17 +0800

    swap case without swapcase() finished

--- swap_case_without_swapcase().py
+++ swap_case_without_swapcase().py
@@ -2,9 +2,24 @@
 # this program will reverse the casing of each of the character of the string
 
 # ask the user for a string
+string = input("Enter a string: ")
+
 # initialize an empty string result
+result = ""
+
 # iterate through each character in the string
-# if the character is lowercase, convert it to uppercase
-# if the character is uppercase, convert it to lowercase
-# append the converted character to the result string
-# print the result string
+for char in string:
+    # if the character is uppercase, convert it to lowercase and add it to result
+    if 'A' <= char <= 'Z': 
+        result += char.lower()
+
+    # if the character is lowercase, convert it to uppercase and add it to result
+    elif 'a' <= char <= 'z':
+        result += char.upper()
+    
+    # if the character is neither, just add it to result
+    else:
+        result += char
+
+# print the result
+print(result, end="")
