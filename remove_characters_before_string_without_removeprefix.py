61dfec0267d27bd10ad97a8778a7c853b642fd4a
unknown <lawrencekulet@gmail.com>
2025-03-28 16:50:26 +0800

    pseudocode

---
+++ remove_characters_before_string_without_removeprefix.py
@@ -0,0 +1,7 @@
+# remove characters before string without removeprefix
+# this program will remove characters at the beginning of a string without removeprefix
+
+# ask the user for a string and a prefix
+# check if the prefix is in the string
+# if it is, remove the prefix from the string
+# otherwise, print the string as is
