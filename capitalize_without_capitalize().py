50a6808341b129f087bf328db3e979574f2de11c
unknown <lawrencekulet@gmail.com>
2025-03-28 19:21:05 +0800

    pseudocode

---
+++ capitalize_without_capitalize().py
@@ -0,0 +1,7 @@
+# capitalize without capitalize()
+# this program will makes the first letter of the string capital letter
+
+# ask the user for a string
+# check if the string is empty
+# if not empty, convert the first letter to uppercase and the rest to lowercase
+# print the result
