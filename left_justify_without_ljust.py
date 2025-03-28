51422c307ee0fb7376fcad82b2f2701abb3578f9
unknown <lawrencekulet@gmail.com>
2025-03-28 18:57:58 +0800

    left justify without ljust finished

--- left_justify_without_ljust.py
+++ left_justify_without_ljust.py
@@ -2,5 +2,8 @@
 # this program will add space characters at the end of the string to complete the number of characters specifies in function parameter
 
 # ask the user for the string and a width
+string = input("Enter a string: ")
+width = int(input("Enter the width: "))
+
 # calculate the number of spaces needed to reach the desired width
-# print the string with the spaces followed by the spaces
+print(string + " " * (width - len(string)), end="")    # print the string with the spaces followed by the spaces
