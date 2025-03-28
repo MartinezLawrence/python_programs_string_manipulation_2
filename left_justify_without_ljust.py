52b6142dab700bde3e362b4256b066dedba8372f
unknown <lawrencekulet@gmail.com>
2025-03-28 18:54:10 +0800

    pseudocode

---
+++ left_justify_without_ljust.py
@@ -0,0 +1,6 @@
+# left justify without ljust
+# this program will add space characters at the end of the string to complete the number of characters specifies in function parameter
+
+# ask the user for the string and a width
+# calculate the number of spaces needed to reach the desired width
+# print the string with the spaces followed by the spaces
