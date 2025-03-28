88ec75470fdb53605166a558ac76df04b89c0212
unknown <lawrencekulet@gmail.com>
2025-03-28 16:40:35 +0800

    remove leading spaces without lstrip finished

--- remove_leading_spaces_without_lstrip.py
+++ remove_leading_spaces_without_lstrip.py
@@ -1,7 +1,15 @@
 # remove leading spaces without lstrip
 # this program will remove leading spaces without lstrip
 
+# ask the user to enter a string
+string = input("Enter a string with leading spaces: ")
+
 # initialize a pointer to 0 
+pointer = 0
+
 # if the pointer is less than the length of the string and the character at the pointer is a space
-# increment the pointer 
-# return the string from the pointer to the end of the string_
+while pointer < len(string) and string[pointer] == ' ':
+    pointer += 1 # increment the pointer
+
+# print the string without leading spaces
+print(string[pointer:]) # print the string from the pointer to the end of the string
