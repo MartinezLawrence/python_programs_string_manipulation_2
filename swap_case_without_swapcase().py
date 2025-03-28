19dba8db4943d7a770cb32e00ee8134a93fa7f58
unknown <lawrencekulet@gmail.com>
2025-03-28 19:16:31 +0800

    pseudocode

---
+++ swap_case_without_swapcase().py
@@ -0,0 +1,10 @@
+# swap case without swapcase()
+# this program will reverse the casing of each of the character of the string
+
+# ask the user for a string
+# initialize an empty string result
+# iterate through each character in the string
+# if the character is lowercase, convert it to uppercase
+# if the character is uppercase, convert it to lowercase
+# append the converted character to the result string
+# print the result string
