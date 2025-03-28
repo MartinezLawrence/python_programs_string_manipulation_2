2c1c54130f858056cb9bcdf4a8a7bbc958136e54
unknown <lawrencekulet@gmail.com>
2025-03-28 18:40:49 +0800

    pseudocode

---
+++ check_if_all_uppercase_without_isupper.py
@@ -0,0 +1,8 @@
+# check if all uppercase without isupper
+# this program will check if all characters is on uppercase without isupper()
+
+# ask the user for a string
+# initialize a flag all_uppercase to True
+# iterate through each character in the string
+# if the character is not uppercase, set all_uppercase to False and break loop
+# print whether all_uppercase is True or False
