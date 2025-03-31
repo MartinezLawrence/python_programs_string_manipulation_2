f08b3dd611e5392c160bea5e7cb05920d48303c3
unknown <lawrencekulet@gmail.com>
2025-03-31 21:09:52 +0800

    pseudocode

---
+++ count_occurences_without_count().py
@@ -0,0 +1,9 @@
+# count occurences without count() 
+# this program return how many time the function parameter appear in the string
+
+# ask the user for a string and a substring to count
+# initialize a counter count to 0
+# iterate through the string, checking for occurences of the substring
+# if found, increment count
+# print count
+
