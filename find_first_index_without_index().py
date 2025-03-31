9f8ea23b6bc8b39d0774a1a482ceb20e32769747
unknown <lawrencekulet@gmail.com>
2025-03-31 21:36:23 +0800

    pseudocode

---
+++ find_first_index_without_index().py
@@ -0,0 +1,7 @@
+# find first index without index()
+# this program will return the first location of the function parameter in the string
+
+# ask the user for a string and a substring to find
+# iterate though the string, checking for the first occurence of the substring
+# if found, print the index and break the loop
+# if not found, print a message indicating that
