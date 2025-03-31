eb4d458138c2cdc50e31727f9d8476d143405061
unknown <lawrencekulet@gmail.com>
2025-03-31 19:48:08 +0800

    pseudocode

---
+++ convert_uppercase_without_upper().py
@@ -0,0 +1,9 @@
+# convert uppercase without upper()
+# this program converts all characters of the string into upper case without upper() function.
+
+# ask the user for a string
+# initialize an empty string to store the result
+# iterate through each character in the string
+# if the character is lowercase, convert it to uppercase by subtracting 32 from its ASCII value
+# append the converted character to the result string(or if it is already uppercase, append it as it is))
+# print the result string
