865b273fdc0f602893c4bef6cbd0f2d909fbe374
unknown <lawrencekulet@gmail.com>
2025-03-31 21:04:42 +0800

    zero fill without zfill() finished

--- zero_fill_without_zfill().py
+++ zero_fill_without_zfill().py
@@ -2,5 +2,11 @@
 # this program add zero characters at the beginning of the string to complete the number of characters specifies in function parameter
 
 # ask the user for a string and a width 
+string = input("Enter a string: ")
+width = int(input("Enter the width: "))
+
 # calculate the number of zeros needed to reach the desired width
+num_zeros = width - len(string)
+
 # print the zeros followed by the string
+print('0' * num_zeros + string, end='')
