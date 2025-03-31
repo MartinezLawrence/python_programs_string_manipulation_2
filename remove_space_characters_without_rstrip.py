5c49f0bb4887cf1a4b6b5f83fdd765ccae26f4e6
unknown <lawrencekulet@gmail.com>
2025-03-31 19:24:24 +0800

    pseudocode

---
+++ remove_space_characters_without_rstrip.py
@@ -0,0 +1,8 @@
+# remove space characters without rstrip 
+# this program will remove the space characters at the end of the string without rstrip
+
+# ask the user for a string
+# initialize a pointer last_char to the last character of the string
+# while last_char is greater than or equal to 0 and the character at last_char is a space
+# decrement last_char by 1
+# print the string from the beginning to last_char + 1
