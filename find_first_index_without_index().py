751f2162405d29f834155bfb98a64da567e660b1
unknown <lawrencekulet@gmail.com>
2025-03-31 21:42:05 +0800

    find first index without index() finished

--- find_first_index_without_index().py
+++ find_first_index_without_index().py
@@ -2,6 +2,16 @@
 # this program will return the first location of the function parameter in the string
 
 # ask the user for a string and a substring to find
+string = input("Enter a string: ")
+substring = input("Enter a substring to find: ")
+
 # iterate though the string, checking for the first occurence of the substring
-# if found, print the index and break the loop
+for index in range(len(string)):
+    if string[index:index + len(substring)] == substring:
+        # if found, print the index and break the loop
+        print(index)
+        break
+
 # if not found, print a message indicating that
+else:
+    print("Substring not found in the string.")
