63b95af42bbc6861681e6e5a1a1ddbe943e11f96
unknown <lawrencekulet@gmail.com>
2025-03-31 20:45:04 +0800

    pseudocode

---
+++ right_justify_without_rjust().py
@@ -0,0 +1,6 @@
+# right justify without rjust()
+# this program add space characters at the beginning of the string to complete the number of characters specifies in function parameter
+
+# ask user for a string and a width
+# calculate the number of spaces needed to reach the desired width
+# print spaces followed by the string
