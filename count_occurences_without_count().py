03fd88fb17253440bc1b202a61eb7e3d6d9c52b0
unknown <lawrencekulet@gmail.com>
2025-03-31 21:31:44 +0800

    count occurences without count() finished

--- count_occurences_without_count().py
+++ count_occurences_without_count().py
@@ -2,8 +2,18 @@
 # this program return how many time the function parameter appear in the string
 
 # ask the user for a string and a substring to count
-# initialize a counter count to 0
-# iterate through the string, checking for occurences of the substring
-# if found, increment count
-# print count
+string = input("Enter a string: ")
+substring = input("Enter a substring to count: ")
+count = 0       # initialize a counter count to 0
+unique = 0
 
+# iterate through the string, checking for occurences of the substring
+while unique < len(string): 
+    if string [unique:unique + len(substring)] == substring:
+        count += 1      # if found, increment count
+        unique += len(substring)
+    else:
+        unique += 1
+
+# print count
+print(count)
