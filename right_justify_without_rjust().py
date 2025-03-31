48dff515879f5101af241e1310aeb87f30bb1d41
unknown <lawrencekulet@gmail.com>
2025-03-31 20:54:44 +0800

    right justify without rjust() finished

--- right_justify_without_rjust().py
+++ right_justify_without_rjust().py
@@ -2,5 +2,8 @@
 # this program add space characters at the beginning of the string to complete the number of characters specifies in function parameter
 
 # ask user for a string and a width
+string = input("Enter a string: ")
+width = int(input("Enter the width: "))
+
 # calculate the number of spaces needed to reach the desired width
-# print spaces followed by the string
+print(' ' * (width - len(string)) + string, end='')    # print spaces followed by the string
