1a56264df3d2523c0c491c346785cbfc7ef3beaf
unknown <lawrencekulet@gmail.com>
2025-03-31 20:36:20 +0800

    pseudocode

---
+++ check_starts_with_without_startswith().py
@@ -0,0 +1,6 @@
+# check starts with without startswith()
+# this program check if the string beginning part matches the function parameter
+
+# ask the user for a string and a prefix
+# compare the first 'n' characters of the string with the prefix(n = lenght of prefix)
+# print whether they match
