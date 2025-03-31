1dcbc08acc38fd9dc3b636cb3fab00208169daa6
unknown <lawrencekulet@gmail.com>
2025-03-31 19:45:09 +0800

    remove suffix without removesuffix() finished

--- remove_suffix_without_removesuffix().py
+++ remove_suffix_without_removesuffix().py
@@ -2,6 +2,13 @@
 # this program will remove the characters at the end of the string that matches the function parameter
 
 # Ask the user for a string and a suffix.
+string = input("Enter a string: ")
+suffix = input("Enter a suffix: ")
+
 # Check if the string ends with the given suffix.
-# If it does, print the substring from the start of the string to the beginning of the suffix.
-# Otherwise, print the original string.
+if string.endswith(suffix):
+    # If it does, print the substring from the start of the string to the beginning of the suffix.
+    print(string[:-len (suffix)])
+
+else:   #  otherwise, print the original string.
+    print(string) 
