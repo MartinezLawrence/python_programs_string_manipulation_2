d55de6201f79f4fc18254190a9e5d3e5913c6996
unknown <lawrencekulet@gmail.com>
2025-03-31 21:59:19 +0800

    pseudocode

---
+++ find_last_index_without_rindex().py
@@ -0,0 +1,7 @@
+# find last index without rindex()
+# this program will return the first location of the function parameter in the string starting from the last character
+
+# ask the user for a string and a substring to find
+# iterate throught the string in reverse, checking for the first occurence of the substring
+# if found, print the index and break the loop
+# if not found, print a message indicating that
