dfff9349e0fee70f762639244f3e1fe5766ca304
unknown <lawrencekulet@gmail.com>
2025-03-31 21:00:11 +0800

    pseudocode

---
+++ zero_fill_without_zfill().py
@@ -0,0 +1,6 @@
+# zero fill without zfill()
+# this program add zero characters at the beginning of the string to complete the number of characters specifies in function parameter
+
+# ask the user for a string and a width 
+# calculate the number of zeros needed to reach the desired width
+# print the zeros followed by the string
