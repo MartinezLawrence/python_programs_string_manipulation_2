772e96934e7ec1fcb3bdab01dd8405eaf14ee89f
unknown <lawrencekulet@gmail.com>
2025-03-31 20:42:21 +0800

    check if starts with without startswith() finished

--- check_starts_with_without_startswith().py
+++ check_starts_with_without_startswith().py
@@ -2,5 +2,8 @@
 # this program check if the string beginning part matches the function parameter
 
 # ask the user for a string and a prefix
+string = input("Enter a string: ")
+prefix = input("Enter a prefix: ")
+
 # compare the first 'n' characters of the string with the prefix(n = lenght of prefix)
-# print whether they match
+print((string[:len(prefix)]) == prefix)   # print whether they match
