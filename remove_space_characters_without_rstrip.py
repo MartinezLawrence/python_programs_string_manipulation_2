12fcdfdecc24275da19244b87c3b915eec7fac58
unknown <lawrencekulet@gmail.com>
2025-03-31 19:32:57 +0800

    remove spaces after string without rstrip() finished

--- remove_space_characters_without_rstrip.py
+++ remove_space_characters_without_rstrip.py
@@ -2,7 +2,15 @@
 # this program will remove the space characters at the end of the string without rstrip
 
 # ask the user for a string
+string = input("Enter a string: ")
+
 # initialize a pointer last_char to the last character of the string
+last_char = len(string) - 1
+
 # while last_char is greater than or equal to 0 and the character at last_char is a space
-# decrement last_char by 1
+while last_char >= 0 and string[last_char] == ' ':
+    # decrement last_char by 1
+    last_char -= 1
+
 # print the string from the beginning to last_char + 1
+print(string[:last_char + 1])
